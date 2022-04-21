cfc32cc6319b11aa4bcdd80b5008a001b06853e3
Norberto Llopiz <nllopiz@gmail.com>
2022-04-21 13:34:51 -0300

    correccion

--- PracticasJava/practicasJava/src/main/java/com/yoprogramo/practicasjava/PracticasJava.java
+++ PracticasJava/practicasJava/src/main/java/com/yoprogramo/practicasjava/PracticasJava.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.yoprogramo.practicasjava;
 
 /**
@@ -15,7 +10,6 @@ import java.util.Scanner; //para carga por teclado
 
 public class PracticasJava {
 
-    @SuppressWarnings("UnusedAssignment")
     public static void main(String[] args) {
 /*
 //**********************Práctica Teoría
