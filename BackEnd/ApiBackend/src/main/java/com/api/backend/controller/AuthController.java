64e43bc969362afda5c8a7bd5885fd4a1af4535c
nllopiz <nllopiz@gmail.com>
2022-06-12 20:55:23 -0300

    integracion token recibido

--- BackEnd/ApiBackend/src/main/java/com/api/backend/controller/AuthController.java
+++ BackEnd/ApiBackend/src/main/java/com/api/backend/controller/AuthController.java
@@ -17,6 +17,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin
 public class AuthController {
     
     @Autowired
--- FrontEndAngular/src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ FrontEndAngular/src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -9,15 +11,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class IniciarSesionComponent implements OnInit {
   form:FormGroup;
 
-  constructor(private formBuilder:FormBuilder) {
+  constructor(
+    private formBuilder:FormBuilder, 
+    private autenticacioService:AutenticacionService,
+    private ruta:Router) {
     this.form=this.formBuilder.group(
       {
-        
+        email:['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]]
       }
     )
   }
     
   ngOnInit(): void {
+  }
+
+  get Email() {
+    return this.form.get('email'); 
+  }
+
+  get Password() {
+    return this.form.get('password');
+  }
+
+  onEnviar(event:Event) {
+    event.preventDefault;
+    this.autenticacioService.IniciarSesion(this.form.value).subscribe(data => {
+      console.log("DATA:" + JSON.stringify(data));
+      this.ruta.navigate(['../acerca-de']);
+    })
   }
 
 }
